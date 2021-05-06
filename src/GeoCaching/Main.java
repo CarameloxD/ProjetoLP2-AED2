dfc5c71d61e021a09a28e04270c66358f414ea74
CarameloxD <joaopedromelo@live.com.pt>
2021-05-06 23:43:51 +0100

    Hash e RedBlacks (inicio).

--- src/GeoCaching/Main.java
+++ src/GeoCaching/Main.java
@@ -1,8 +1,27 @@
 package GeoCaching;
 
-import java.util.ArrayList;
+import edu.princeton.cs.algs4.RedBlackBST;
+import edu.princeton.cs.algs4.SeparateChainingHashST;
 
 public class Main {
+
+    //Path
+    public String inputxt = (".//data//input.txt");
+    public String utilizadortxt = (".//data//utilizador.txt");
+    public String cachestxt = (".//data//caches.txt");
+    public String logxt = (".//data//log.txt");
+    public String travelBugxt = (".//data//travelBug.txt");
+
+    public static SeparateChainingHashST<String, Basic> basics = new SeparateChainingHashST<>();
+    public static SeparateChainingHashST<String, Admin> admins = new SeparateChainingHashST<>();
+    public static SeparateChainingHashST<String, Premium> premiums = new SeparateChainingHashST<>();
+    public static SeparateChainingHashST<String, Cache> caches = new SeparateChainingHashST<>();
+    public static RedBlackBST<String, TravelBug> travelbugs = new RedBlackBST<>();
+    public static RedBlackBST<String, Log> logs = new RedBlackBST<>();
+
+    //Metodo interacao
+
+
     public static void main(String[] args) {
 
     }
